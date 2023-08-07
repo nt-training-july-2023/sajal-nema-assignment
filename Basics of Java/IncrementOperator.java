0b797a437d99861b385fe067c8f138e5db6543f9
Sajal-NucleusTeq <139866050+Sajal-NucleusTeq@users.noreply.github.com>
2023-08-07 20:14:12 +0530

    Delete Basics of Java directory

--- Basics of Java/IncrementOperator.java
+++
@@ -1,9 +0,0 @@
-public class IncrementOperator {
-    public static void main(String[] args) {
-        int preInc = 2;
-        System.out.println(++preInc);       //3
-
-        int postInc = 2;
-        System.out.println(postInc++);      //2        
-    }
-}
