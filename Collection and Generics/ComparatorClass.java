06ca33ac296ce53de166aa4d2ffe2e3dff01551d
Sajal-NucleusTeq <139866050+Sajal-NucleusTeq@users.noreply.github.com>
2023-08-07 20:14:26 +0530

    Delete Collection and Generics directory

--- Collection and Generics/ComparatorClass.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-
-public class ComparatorClass implements Comparator<Employee>{
-    @Override
-    public int compare(Employee obj1 ,Employee obj2){
-        return Math.max(obj1.getEmpId() - obj2.getEmpId() , obj1.getName().compareTo(obj2.getName()));
-    }
-
-}
