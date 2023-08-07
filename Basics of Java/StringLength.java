98853fc31112e5edd74d12d40824701e057ba626
Sajal-NucleusTeq <139866050+Sajal-NucleusTeq@users.noreply.github.com>
2023-08-07 20:15:30 +0530

    Delete Basics of Java directory

--- Basics of Java/StringLength.java
+++
@@ -1,7 +0,0 @@
-public class StringLength {
-        public static void main(String[] args){
-            String s = "Hello";
-            
-            System.out.println("String Length is :" + s.length());
-        }
-}
