c9def1ede25f1bd45633a222b7515b0b72422841
Sajal-NucleusTeq <139866050+Sajal-NucleusTeq@users.noreply.github.com>
2023-08-14 11:18:14 +0530

    Delete OOPs directory

--- OOPs/FinalVariable.java
+++
@@ -1,9 +0,0 @@
-public class FinalVariable {
-    public static void main(String[] args) {
-        final float PI = 3.14f;
-        System.out.println("The Value of PI is : " + PI);
-
-        //Reinitialize Final Variable
-        // PI = 23;      //Error: The final local variable PI cannot be assigned. It must be blank and not using a compound assignment 
-    }
-}
